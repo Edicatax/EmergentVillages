ffbaa05b83378d734379f5a661de9913ad28d2ef
Edicatad <0xdavy@gmail.com>
2017-03-16 17:03:50 +0100

    Removed jump event handler

--- src/main/java/com/edicatad/emvi/handlers/TickHandler.java
+++ src/main/java/com/edicatad/emvi/handlers/TickHandler.java
@@ -1,7 +1,5 @@
 package com.edicatad.emvi.handlers;
 
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraftforge.event.entity.living.LivingEvent.LivingJumpEvent;
 import net.minecraftforge.event.world.ChunkWatchEvent.UnWatch;
 import net.minecraftforge.event.world.ChunkWatchEvent.Watch;
 import net.minecraftforge.fml.common.FMLLog;
@@ -11,24 +9,6 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 @Mod.EventBusSubscriber
 public class TickHandler {
-	public static void init(){
-		
-	}
-
-
-	@SubscribeEvent(priority=EventPriority.HIGHEST, receiveCanceled=true)
-	
-	public static void onEvent(LivingJumpEvent event)
-	{
-	    // DEBUG
-	    if (event.getEntity() instanceof EntityPlayer)
-	    {
-	        FMLLog.getLogger().info("BOING");
-	    }
-	        
-	}
-
-
 	@SubscribeEvent(priority=EventPriority.NORMAL)
 	public static void chunkWatched(Watch event){
 		FMLLog.getLogger().info("[EMVI]  Chunk watched at coords %i, %i", event.getChunk().chunkXPos, event.getChunk().chunkZPos);
--- src/main/java/com/edicatad/emvi/proxy/CommonProxy.java
+++ src/main/java/com/edicatad/emvi/proxy/CommonProxy.java
@@ -1,14 +1,11 @@
 package com.edicatad.emvi.proxy;
 
-import com.edicatad.emvi.handlers.TickHandler;
-
 public class CommonProxy implements IProxy{
 	// This code runs server-side
 	// Remember that Minecraft singleplayer loads a local server in the background
 	@Override
 	public void preInit() {
 		// TODO Auto-generated method stub
-		TickHandler.init();
 	}
 
 	@Override
