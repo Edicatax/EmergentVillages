ea884b4c12f27480879721cd47f2c3d5ef4de03f
Edicatad <0xdavy@gmail.com>
2017-03-16 20:16:22 +0100

    added initial event code for villager spawning

--- src/main/java/com/edicatad/emvi/handlers/TickHandler.java
+++ src/main/java/com/edicatad/emvi/handlers/TickHandler.java
@@ -6,6 +6,7 @@ import net.minecraftforge.fml.common.FMLLog;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent.WorldTickEvent;
 
 @Mod.EventBusSubscriber
 public class TickHandler {
@@ -17,5 +18,24 @@ public class TickHandler {
 	@SubscribeEvent(priority=EventPriority.NORMAL)
 	public static void chunkUnwatched(UnWatch event){
 		FMLLog.getLogger().info("[EMVI]  Chunk unwatched at coords %i, %i", event.getChunk().chunkXPos, event.getChunk().chunkZPos);
+	}
+	
+	@SubscribeEvent
+	public static void worldTick(WorldTickEvent event){
+		switch(event.phase){
+		case END:
+			break;
+		case START:
+			/*
+			 * TODO 
+			 * figure out which dimension this world is in, I want to only spawn villagers in the overworld
+			 * use getWorldTime to only run spawn checker code at dawn or something
+			 * spawn villagers based on random chance
+			 */
+			//event.world.getWorldTime();
+			break;
+		default:
+			break;
+		}
 	}
 }
